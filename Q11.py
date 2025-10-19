9fae2016918d656ba0937be1dc6d736f5de8e429
Vasu Develam <vasudevelam@gmail.com>
2025-10-19 12:15:31 +0530

    Delete Q1 to Q9 files

--- Q11.py
+++
@@ -1,9 +0,0 @@
-#Q11 Fibonacci Series up to n terms
-def facb(n):
-    a=0
-    b=1
-    for i in range(n):
-        print(a,end=" ")
-        a,b=b,a+b
-        
-facb(10)
--- Q12.py
+++
@@ -1,9 +0,0 @@
-# Q12 Check if a Person is Adult or Minor
-def name_age(name,age):
-    if age>18:
-        print("Adult :",name,"Age: ",age)
-    else:
-        print("Minor",name,"Age: ",age)
-
-
-name_age("Vasu",19)
